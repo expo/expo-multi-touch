66a1d5735de1088aeb636b6404d627aa956cab06
Evan Bacon <baconbrix@gmail.com>
2018-03-12 13:24:25 -0700

    updated example

--- examples/basic/App.js
+++ examples/basic/App.js
@@ -2,46 +2,94 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { MultiTouchView } from 'expo-multi-touch';
 
+const colors = ['red', 'blue', 'yellow', 'green', 'orange', 'cyan', 'plum', 'gray', 'purple'];
 export default class App extends Component {
+  state = {
+    touches: {},
+  };
+
+  touchProps = {
+    onTouchBegan: event => {
+      const { identifier } = event;
+      this.setState(previous => ({
+        touches: {
+          ...previous.touches,
+          [identifier]: event,
+        },
+      }));
+    },
+    onTouchMoved: event => {
+      const { identifier } = event;
+      this.setState(previous => ({
+        touches: {
+          ...previous.touches,
+          [identifier]: event,
+        },
+      }));
+    },
+    onTouchEnded: event => {
+      const { identifier, deltaX, deltaY, isTap } = event;
+      this.setState(previous => ({
+        touches: {
+          ...previous.touches,
+          [identifier]: null,
+        },
+      }));
+    },
+    onTouchCancelled: event => {
+      const { identifier, deltaX, deltaY, isTap } = event;
+      this.setState(previous => ({
+        touches: {
+          ...previous.touches,
+          [identifier]: null,
+        },
+      }));
+    },
+    onTouchesBegan: () => {
+      console.log('onTouchesBegan');
+    },
+    onTouchesMoved: () => {},
+    onTouchesEnded: () => {
+      console.log('onTouchesEnded');
+    },
+    onTouchesCancelled: () => {
+      console.log('onTouchesCancelled');
+    },
+  };
+
   render() {
-    const props = {
-      onTouchBegan: event => {
-        const { identifier } = event;
-        // event
-        console.log('onTouchBegan', identifier);
-      },
-      onTouchMoved: () => {
-        // console.log('onTouchMoved');
-      },
-      onTouchEnded: event => {
-        const { identifier, deltaX, deltaY, isTap } = event;
-        console.log('onTouchEnded', identifier, deltaX, isTap);
-      },
-      onTouchCancelled: event => {
-        const { identifier, deltaX, deltaY, isTap } = event;
-        console.log('onTouchCancelled', identifier, deltaX, isTap);
-      },
-      onTouchesBegan: () => {
-        console.log('onTouchesBegan');
-      },
-      onTouchesMoved: () => {
-        // console.log('onTouchesMoved');
-      },
-      onTouchesEnded: () => {
-        console.log('onTouchesEnded');
-      },
-      onTouchesCancelled: () => {
-        console.log('onTouchesCancelled');
-      },
-    };
+    const { touches } = this.state;
     return (
       <View style={{ flex: 1, backgroundColor: 'orange' }}>
-        <MultiTouchView style={{ flex: 1 }} {...props}>
+        <MultiTouchView style={{ flex: 1 }} {...this.touchProps}>
           <View style={styles.container}>
-            <Text style={styles.paragraph}>
-              Change code in the editor and watch it change on your phone! Save
-              to get a shareable url.
-            </Text>
+            <Text style={styles.paragraph}>Touch around...</Text>
+
+            {Object.values(touches).map((item, index) => {
+              if (!item) {
+                return null;
+              }
+              console.log(index, item.locationX);
+
+              return (
+                <View
+                  key={index}
+                  style={[
+                    styles.touch,
+                    {
+                      transform: [
+                        { translateX: -TOUCH_SIZE / 2 },
+                        { translateY: -TOUCH_SIZE / 2 },
+                        { scale: 1 + (item.force || 0) * 2 },
+                      ],
+                      backgroundColor: colors[index % colors.length],
+                      top: item.pageY,
+                      left: item.pageX,
+                    },
+                  ]}
+                />
+              );
+            })}
           </View>
         </MultiTouchView>
       </View>
@@ -49,12 +97,20 @@ export default class App extends Component {
   }
 }
 
+const TOUCH_SIZE = 56;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
+  },
+  touch: {
+    position: 'absolute',
+    aspectRatio: 1,
+    width: TOUCH_SIZE,
+    borderRadius: TOUCH_SIZE / 2,
   },
   paragraph: {
     margin: 24,
